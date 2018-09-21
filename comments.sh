93a7f28effb13b41f82f7970998f84c70d140029
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-21 12:22:29 +0100

    Adds in comments.sh

---
+++ comments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This line is intended to be used as a general description of the script
+# and anything that it does
+
+clear    # Clears the screen
+
+MYUSERNAME="Terry" # The username for this application
+
+echo "We are using the default user called: $MYUSERNAME" # Display to console
+
+DATETIMESTAMP=`date`
+
+echo "This is when the script was run: $DATETIMESTAMP" # This is the timestamp of run
