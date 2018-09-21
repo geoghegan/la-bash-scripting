4301e52c7bb27ce2ab493d99a774eab6793277c8
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-21 14:56:21 +0100

    Adds in a bit more varables

--- readsample.sh
+++ readsample.sh
@@ -3,5 +3,14 @@
 
 echo "Enter your first name: "
 read FIRSTNAME
+echo "Enter your last name: "
+read LASTNAME
 
-echo "Hello, $FIRSTNAME"
+echo ""
+echo "Your full name is: $FIRSTNAME $LASTNAME"
+echo ""
+
+echo "Enter your age: "
+read USERAGE
+
+echo "In 10 years, you will be `expr $USERAGE + 10` years old."
