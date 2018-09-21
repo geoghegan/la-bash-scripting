5c9cb738f901503496f020659e9a09bf4c5a6b1e
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-21 12:59:30 +0100

    Adds in aliases

--- substitution.sh
+++ substitution.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # This script is intended to show how to do simple substitution
 
+alias TODAY="date"
+alias UFILES="find /home -user user"
+
 TODAYSDATE=`date`
 USERFILES=`find /home -user user`
 
 echo "Today's date: $TODAYSDATE"
 echo "All files own by USER: $USERFILES"
+
+A=`TODAY`
+B=`UFILES`
+echo "With Alias, TODAY is: $A"
+echo "With Alias, UFILES is: $B"
