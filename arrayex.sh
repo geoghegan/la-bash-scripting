a01353c2aeaa36fc452f0d8cb6023a47c20c6ea4
Nick Geoghegan <nick.geoghegan@airbnb.com>
2018-09-21 15:40:42 +0100

    Adds in arrayex.sh

---
+++ arrayex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Simple array list and loop for display
+
+SERVERLIST=("websrv01" "websrv02" "websrv03" "websvr04" )
+COUNT=0
+
+for INDEX in ${SERVERLIST[@]}; do
+  echo "Processing Server: ${SERVERLIST[COUNT]}"
+  COUNT="`expr $COUNT + 1`"
+done
